747320fadac065cac25a1c7f284e544ae1376178
larkspur3 <12821562+larkspur3@users.noreply.github.com>
2022-04-30 22:20:04 -0600

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,39 @@
+project "imgui"
+	kind "StaticLib"
+	language "C++"
+	staticruntime "off"
+
+	targetdir("bin/" .. outputDir .. "/%{prj.name}")
+	objdir("obj/" .. outputDir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configuration:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configuration:Dist"
+		runtime "Release"
+		optimize "on"
+		symbols "off"
